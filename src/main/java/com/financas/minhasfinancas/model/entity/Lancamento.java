8bef4f4759cf9c4079b769919cc42b29ae891468
francisco.xavier <francisco.xavier@dataprev.gov.br>
2022-06-14 15:41:00 -0300

    Dia 01

---
+++ src/main/java/com/financas/minhasfinancas/model/entity/Lancamento.java
@@ -0,0 +1,24 @@
+package com.financas.minhasfinancas.model.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table( name = "lancamento", schema = "financas")
+public class Lancamento {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id")
+	private Long id;
+	
+	@Column(name = "mes")
+	private Integer mes;
+	
+	@Column(name = "ano")
+	private Integer ano;
+}
