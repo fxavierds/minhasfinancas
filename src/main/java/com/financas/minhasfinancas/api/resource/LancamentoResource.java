e12ba39dc178e42c44f41412478f450af6e0f8e1
Xavier <fxavierds@yahoo.com.br>
2023-08-02 21:13:30 -0300

    dto

--- src/main/java/com/financas/minhasfinancas/api/resource/LancamentoResource.java
+++ src/main/java/com/financas/minhasfinancas/api/resource/LancamentoResource.java
@@ -2,7 +2,6 @@ package com.financas.minhasfinancas.api.resource;
 
 import java.util.List;
 import java.util.Optional;
-
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -45,19 +44,18 @@ public class LancamentoResource {
 		}
 
 	}
-	
+
 	@GetMapping
 	public ResponseEntity<Object> buscar(@RequestParam(value = "descricao", required = false) String descricao,
 			@RequestParam(value = "mes", required = false) Integer mes,
-			@RequestParam(value = "ano", required = false) Integer ano,
-			@RequestParam("usuario") Long idUsuario) {
-		
+			@RequestParam(value = "ano", required = false) Integer ano, @RequestParam("usuario") Long idUsuario) {
+
 		Lancamento lancamentoFiltro = new Lancamento();
 		lancamentoFiltro.setAno(ano);
 		lancamentoFiltro.setMes(mes);
 		lancamentoFiltro.setDescricao(descricao);
 		Optional<Usuario> usuario = usuarioService.obterPorId(idUsuario);
-		if(usuario.isPresent()) {
+		if (usuario.isPresent()) {
 			lancamentoFiltro.setUsuario(usuario.get());
 		} else {
 			return ResponseEntity.badRequest().body("Usuario não encontrado");
@@ -66,9 +64,18 @@ public class LancamentoResource {
 		return ResponseEntity.ok(lancamentos);
 	}
 
-	@SuppressWarnings({ "rawtypes", "unchecked" })
+	@SuppressWarnings({ "unchecked", "rawtypes" })
+	@GetMapping("{id}")
+	public ResponseEntity<?> obterLancamento(@PathVariable("id") Long id) {
+		return service.obterPorId(id)
+				.map(lancamento -> new ResponseEntity(converter(lancamento), HttpStatus.OK))
+				.orElseGet(() -> new ResponseEntity<Object>(HttpStatus.NOT_FOUND));
+
+	}
+
+	@SuppressWarnings({ })
 	@PutMapping("{id}")
-	public ResponseEntity atualizar(@PathVariable("id") Long id, @RequestBody LancamentoDTO dto) {
+	public ResponseEntity<?> atualizar(@PathVariable("id") Long id, @RequestBody LancamentoDTO dto) {
 		return service.obterPorId(id).map(entity -> {
 			try {
 				Lancamento lancamento = converter(dto);
@@ -79,12 +86,12 @@ public class LancamentoResource {
 				return ResponseEntity.badRequest().body(e.getMessage());
 			}
 
-		}).orElseGet(() -> new ResponseEntity("Lançamento não encontrado", HttpStatus.BAD_REQUEST));
+		}).orElseGet(() -> new ResponseEntity<Object>("Lançamento não encontrado", HttpStatus.BAD_REQUEST));
 	}
-	
-	@SuppressWarnings({ "rawtypes", "unchecked" })
+
+	@SuppressWarnings({ })
 	@PutMapping("{id}/atualiza-status")
-	public ResponseEntity atualizarStatus(@PathVariable("id") Long id, @RequestBody AtualizaStatusDTO dto) {
+	public ResponseEntity<?> atualizarStatus(@PathVariable("id") Long id, @RequestBody AtualizaStatusDTO dto) {
 		return service.obterPorId(id).map(entity -> {
 			StatusLancamento statusSelecionado = StatusLancamento.valueOf(dto.getStatus());
 			if (statusSelecionado == null) {
@@ -97,19 +104,26 @@ public class LancamentoResource {
 			} catch (RegraNegocioException e) {
 				return ResponseEntity.badRequest().body(e.getMessage());
 			}
-		}).orElseGet(() -> new ResponseEntity("Lançamento não encontrado", HttpStatus.BAD_REQUEST));
-		
+		}).orElseGet(() -> new ResponseEntity<Object>("Lançamento não encontrado", HttpStatus.BAD_REQUEST));
+
 	}
-	
-	@SuppressWarnings({ "unchecked", "rawtypes" })
+
+	@SuppressWarnings({ })
 	@DeleteMapping("{id}")
-	public ResponseEntity deletar(@PathVariable("id") Long id) {
+	public ResponseEntity<?> deletar(@PathVariable("id") Long id) {
 		return service.obterPorId(id).map(entity -> {
 			service.deletar(entity);
-			return new ResponseEntity(HttpStatus.NO_CONTENT);
-		}).orElseGet(() -> new ResponseEntity("Lançamento não encontrado", HttpStatus.BAD_REQUEST));
+			return new ResponseEntity<Object>(HttpStatus.NO_CONTENT);
+		}).orElseGet(() -> new ResponseEntity<Object>("Lançamento não encontrado", HttpStatus.BAD_REQUEST));
 	}
-	
+
+	private LancamentoDTO converter(Lancamento lancamento) {
+		return LancamentoDTO.builder().ano(lancamento.getAno()).descricao(lancamento.getDescricao())
+				.id(lancamento.getId()).mes(lancamento.getMes()).status(lancamento.getStatus().name())
+				.tipo(lancamento.getTipo().name()).valor(lancamento.getValor()).usuario(lancamento.getUsuario().getId())
+				.build();
+	}
+
 	private Lancamento converter(LancamentoDTO dto) {
 		Lancamento lancamento = new Lancamento();
 		lancamento.setId(dto.getId());
@@ -122,13 +136,13 @@ public class LancamentoResource {
 				.orElseThrow(() -> new RegraNegocioException("Usuario não encontrado"));
 
 		lancamento.setUsuario(usuario);
-		if(dto.getTipo() != null) {
+		if (dto.getTipo() != null) {
 			lancamento.setTipo(TipoLancamento.valueOf(dto.getTipo()));
 		}
-		if(dto.getStatus() != null) {
+		if (dto.getStatus() != null) {
 			lancamento.setStatus(StatusLancamento.valueOf(dto.getStatus()));
 		}
-		
+
 		return lancamento;
 
 	}
