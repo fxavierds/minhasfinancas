b566d452f1814a53c6928a349ff817a5692b7721
Francisco <fxavierds@yahoo.com.br>
2024-04-30 23:06:51 -0300

    Spring secutity

--- src/main/java/com/financas/minhasfinancas/service/impl/UsuarioServiceImpl.java
+++ src/main/java/com/financas/minhasfinancas/service/impl/UsuarioServiceImpl.java
@@ -5,6 +5,7 @@ import java.util.Optional;
 import javax.transaction.Transactional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import com.financas.minhasfinancas.exceptions.ErroAutenticacao;
@@ -16,11 +17,13 @@ import com.financas.minhasfinancas.service.UsuarioService;
 @Service
 public class UsuarioServiceImpl implements UsuarioService{
 	private UsuarioRepository repository;
+	private PasswordEncoder enconder;
 	
 	@Autowired
-	public UsuarioServiceImpl(UsuarioRepository usuarioRepository) {
+	public UsuarioServiceImpl(UsuarioRepository usuarioRepository, PasswordEncoder enconder) {
 		super();
 		this.repository = usuarioRepository;
+		this.enconder = enconder;
 	}
 
 	@Override
@@ -30,7 +33,9 @@ public class UsuarioServiceImpl implements UsuarioService{
 			throw new ErroAutenticacao("Usuário não encontrado.");
 		}
 		
-		if(!usuario.get().getSenha().equals(senha)) {
+		boolean senhasBatem = enconder.matches(senha, usuario.get().getSenha());
+		
+		if(!senhasBatem) {
 			throw new ErroAutenticacao("Senha inválida.");
 		}
 		return usuario.get();
@@ -40,7 +45,14 @@ public class UsuarioServiceImpl implements UsuarioService{
 	@Transactional
 	public Usuario salvarUsuario(Usuario usuario) {
 		validarEmail(usuario.getEmail());
+		criptografarSenha(usuario);
 		return repository.save(usuario);
+	}
+
+	private void criptografarSenha(Usuario usuario) {
+		String senha = usuario.getSenha();
+		String senhaCrypto = enconder.encode(senha);
+		usuario.setSenha(senhaCrypto);
 	}
 
 	@Override
